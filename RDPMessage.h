7bfba7da6d1732b4c464f53ee2528fe00b5da1b4
Callum <callumcolvine@gmail.com>
2017-03-10 12:39:41 -0800

    Added to DesignDoc and made small additions to client and server

--- RDPMessage.h
+++ RDPMessage.h
@@ -7,6 +7,23 @@ class RDPMessage
 {
   public:
   	std::string toString();
+  	// Getters
+  	std::string magic() { return _magic; }
+  	int type(){ return _type; }
+  	int seqNum(){ return _seqNum; }
+  	int ackNum(){ return _ackNum; }
+  	int length(){ return _length; }
+  	int size(){ return _size; }
+  	std::string message() { return _message; }
+  	// Setters
+  	void setMagic(std::string magicIn) { _magic = magicIn; }
+  	void setType(int typeIn) { _type = typeIn; }
+  	void setSeqNum(int seqNumIn) { _seqNum = seqNumIn; }
+  	void setAckNum(int ackNumIn) { _ackNum = ackNumIn; }
+  	void setLength(int lengthIn) { _length = lengthIn; }
+  	void setSize(int sizeIn) { _size = sizeIn; }
+  	void setMessage(std::string messageIn) { _message = messageIn; }
+  	// Contructor/destructor
 	RDPMessage();
 	~RDPMessage();
   
@@ -17,6 +34,7 @@ class RDPMessage
   	int _ackNum;			// Acknowledgement Number
   	int _length;			// Payload length
   	int _size;				// Window size
+  	std::string _message;
 };
 
 #endif
